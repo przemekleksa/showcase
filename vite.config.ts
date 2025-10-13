04350aaadb602854cbc64171b390c45d8740b798
Przemyslaw Leksa <przemyslaw.leksa@kitopi.com>
2025-10-13 22:06:08 +0200

    fix: configure Vite to use different base paths for development and production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/showcase/',
+  base: process.env.NODE_ENV === 'production' ? '/showcase/' : '/',
   css: {
     modules: {
       localsConvention: 'camelCase',
