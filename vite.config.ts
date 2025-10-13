49ea870606eff298cd3eaa7f6fdb8028dbb6d891
Przemyslaw Leksa <przemyslaw.leksa@kitopi.com>
2025-10-13 19:43:20 +0200

    feat: ghPages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/showcase/' : '/',
   css: {
     modules: {
       localsConvention: 'camelCase',
