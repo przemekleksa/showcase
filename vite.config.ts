4b7190871c0860b8b01264634de14f1b03c2b9c4
Przemyslaw Leksa <przemyslaw.leksa@kitopi.com>
2025-10-13 19:52:57 +0200

    feat: ghp2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/showcase/' : '/',
+  base: '/showcase/',
   css: {
     modules: {
       localsConvention: 'camelCase',
